523c744db9f390a8890db16aee42c44878d7e3e7
tomy224 <parmenara@gmail.com>
2025-08-31 00:48:13 +0900

    for文を理解中(pythonと少し違うが理解した)

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
-const array = [1,2,3]
-console.log(array)
+// const array = [1,2,3]
+// console.log(array)
 
-console.log(array[2]);
+// console.log(array[2]);
 
-const firstPokemons = ["nyaoha","hoge-ta"]
-console.log(firstPokemons)
-console.log(firstPokemons[1]);
+// const firstPokemons = ["nyaoha","hoge-ta"]
+// console.log(firstPokemons)
+// console.log(firstPokemons[1]);
 
+const questions = [
+  "What is your name?",
+  "What is your favorite color?",
+  "What is your quest?"
+];
+
+for (let i = 0; i < questions.length; i++) {
+  console.log(questions[i]);
+}
